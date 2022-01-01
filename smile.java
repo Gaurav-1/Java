3348a3196c654999dfc89614ce671501aeac8fb1
Gaurav-1 <56622722+Gaurav-1@users.noreply.github.com>
2022-01-01 17:39:49 +0530

    26/12/2021

--- smile.java
+++ smile.java
@@ -1,23 +1,27 @@
 import java.applet.Applet;
 import java.awt.*;
+
 //<Applet code="smile" height=800 width=800></Applet>
 
 public class smile extends Applet {
 	
 	public void paint(Graphics g)
 	{
+		//face
 		g.drawOval(70, 50, 250, 250);
-		
+		//left eye
 		g.drawOval(130, 100, 25, 30);
 		g.fillOval(133, 110, 20, 20);
-		
+		//right eye
 		g.drawOval(222, 100, 25, 30);
 		g.fillOval(225, 110, 20, 20);
-		
+		//smile
 		g.drawArc(140, 180, 110, 60, 180, 180);
-		g.drawArc(290, 130, 50, 50, 281, 177);
+		//left ear
 		g.drawArc(50, 130, 50, 50, 85, 175);
-		
+		//right ear
+		g.drawArc(290, 130, 50, 50, 281, 177);
+		//hairs
 		g.drawLine(190, 51, 170, 25);
 		g.drawLine(190, 51, 190, 25);
 		g.drawLine(190, 51, 210, 25);
